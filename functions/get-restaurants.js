8d7e07505fb4a5ac67ce07dd5a92b0e87dcad3af
Stephen Kilbourn <skilbourn@element84.com>
2023-01-18 16:17:09 -0600

    workshop week 2 add get-restaurants dynamic config

--- functions/get-restaurants.js
+++ functions/get-restaurants.js
@@ -1,7 +1,9 @@
+const middy = require('@middy/core')
+const ssm = require('@middy/ssm')
 const DocumentClient = require('aws-sdk/clients/dynamodb').DocumentClient
 const dynamodb = new DocumentClient()
 
-const defaultResults = process.env.defaultResults || 8
+const { serviceName, stage } = process.env
 const tableName = process.env.restaurants_table
 
 const getRestaurants = async (count) => {
@@ -16,12 +18,19 @@ const getRestaurants = async (count) => {
   return resp.Items
 }
 
-module.exports.handler = async (event, context) => {  
-  const restaurants = await getRestaurants(defaultResults)
+module.exports.handler = middy(async (event, context) => {
+  const restaurants = await getRestaurants(context.config.defaultResults)
   const response = {
     statusCode: 200,
     body: JSON.stringify(restaurants)
   }
 
   return response
-}
+}).use(ssm({
+  cache: true,
+  cacheExpiry: 1 * 60 * 1000, // 1 mins
+  setToContext: true,
+  fetchData: {
+    config: `/${serviceName}/${stage}/get-restaurants/config`
+  }
+}))
