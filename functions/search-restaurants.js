3085013784fa51820295f78b773091072881530e
Stephen Kilbourn <skilbourn@element84.com>
2023-01-18 21:06:07 -0600

    re-add middy on search-restaurants without cache

--- functions/search-restaurants.js
+++ functions/search-restaurants.js
@@ -1,8 +1,11 @@
+const middy = require('@middy/core')
+const ssm = require('@middy/ssm')
+const { metricScope, Unit } = require('aws-embedded-metrics')
 const DocumentClient = require('aws-sdk/clients/dynamodb').DocumentClient
 
 const dynamodb = new DocumentClient()
 
-const defaultResults = process.env.defaultResults || 8
+const { serviceName, stage } = process.env
 const tableName = process.env.restaurants_table
 
 const findRestaurantsByTheme = async (theme, count) => {
@@ -19,15 +22,29 @@ const findRestaurantsByTheme = async (theme, count) => {
   return resp.Items
 }
 
-module.exports.handler = async (event, context) => {
+module.exports.handler = middy(
+  metricScope(metrics =>
+    async (event, context) => {
+      metrics.setNamespace('prod-serverless-workshop')
+      metrics.putDimensions({ Service: "lambda-emf-demo" })
+      const start = Date.now()
+      const req = JSON.parse(event.body)
+      const theme = req.theme
+      const restaurants = await findRestaurantsByTheme(theme, context.config.defaultResults)
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(restaurants)
+      }
+      const end = Date.now()
 
-    const req = JSON.parse(event.body)
-    const theme = req.theme
-    const restaurants = await findRestaurantsByTheme(theme, defaultResults)
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(restaurants)
+      metrics.putMetric("latency", end - start, Unit.Milliseconds)
+      metrics.setProperty("RequestId", context?.awsRequestId)
+      metrics.setProperty("ApiGatewayRequestId", event.requestContext?.requestId)
+
+      return response
+  })).use(ssm({
+    setToContext: true,
+    fetchData: {
+      config: `/${serviceName}/${stage}/search-restaurants/config`
     }
-
-    return response
-};
+  }))
