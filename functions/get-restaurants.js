36553cacade9581b064627e256c96e1d6ebde207
Stephen Kilbourn <skilbourn@element84.com>
2023-01-18 19:07:23 -0600

    re-remove middy on get-restaurants

--- functions/get-restaurants.js
+++ functions/get-restaurants.js
@@ -1,10 +1,7 @@
-const middy = require('@middy/core')
-const ssm = require('@middy/ssm')
-
 const DocumentClient = require('aws-sdk/clients/dynamodb').DocumentClient
 const dynamodb = new DocumentClient()
 
-const { serviceName, stage } = process.env
+const defaultResults = process.env.defaultResults || 8
 const tableName = process.env.restaurants_table
 
 const getRestaurants = async (count) => {
@@ -19,19 +16,12 @@ const getRestaurants = async (count) => {
   return resp.Items
 }
 
-module.exports.handler = middy(async (event, context) => {
-  const restaurants = await getRestaurants(context.config.defaultResults)
+module.exports.handler = async (event, context) => {
+  const restaurants = await getRestaurants(defaultResults)
   const response = {
     statusCode: 200,
     body: JSON.stringify(restaurants)
   }
 
   return response
-}).use(ssm({
-  cache: true,
-  cacheExpiry: 1 * 60 * 1000, // 1 mins
-  setToContext: true,
-  fetchData: {
-    config: `/${serviceName}/${stage}/get-restaurants/config`
-  }
-}))
+}
